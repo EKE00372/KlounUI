0b5c572fb6550b8826c4c0aabe674e523ef24110
EK <eke00372@gmail.com>
2026-07-06 02:48:07 +0800

    Update tutorial, remove old cvar

--- !Anyon/BlizzFix/HideTutorial.lua
+++ !Anyon/BlizzFix/HideTutorial.lua
@@ -1,28 +1,21 @@
-local pendingChanges
+-- Fallback values are used only if Blizzard globals are unavailable.
+local NUM_LE_FRAME_TUTORIALS = NUM_LE_FRAME_TUTORIALS or 163
+local NUM_LE_FRAME_TUTORIAL_ACCCOUNTS = NUM_LE_FRAME_TUTORIAL_ACCCOUNTS or 49
 
--- VARIABLES_LOADED seems to consistently fire after ADDON_LOADED
-local function OnEvent(self, event, addon)
-	if event == "ADDON_LOADED" and addon == "HideTutorial" then
-		local tocVersion = select(4, GetBuildInfo())
-		if not HideTutorialDB2 or HideTutorialDB2 < tocVersion then
-			-- only do this once per character
-			HideTutorialDB2 = tocVersion
-			pendingChanges = true
+local function OnEvent()
+	C_CVar.SetCVar("showTutorials", 0)
+	C_CVar.SetCVar("showNPETutorials", 0)
+
+	-- Avoid rewriting the whole bitfield if the last known tutorial is already closed.
+	if not C_CVar.GetCVarBitfield("closedInfoFrames", NUM_LE_FRAME_TUTORIALS) then
+		for i = 1, NUM_LE_FRAME_TUTORIALS do
+			C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
 		end
-	elseif event == "VARIABLES_LOADED" then
-		C_CVar.SetCVar("showTutorials", 0)
-		C_CVar.SetCVar("showNPETutorials", 0)
-		C_CVar.SetCVar("hideAdventureJournalAlerts", 1)
-		--C_CVar.RegisterCVar("hideHelptips", 1) -- this can actually block interaction with mission tables
-		local lastInfoFrame = C_CVar.GetCVarBitfield("closedInfoFrames", NUM_LE_FRAME_TUTORIALS)
-		if pendingChanges or not lastInfoFrame then
-			-- help plates
-			for i = 1, NUM_LE_FRAME_TUTORIALS do
-				C_CVar.SetCVarBitfield("closedInfoFrames", i, true)
-			end
-			for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
-				C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
-			end
+	end
+
+	if not C_CVar.GetCVarBitfield("closedInfoFramesAccountWide", NUM_LE_FRAME_TUTORIAL_ACCCOUNTS) then
+		for i = 1, NUM_LE_FRAME_TUTORIAL_ACCCOUNTS do
+			C_CVar.SetCVarBitfield("closedInfoFramesAccountWide", i, true)
 		end
 	end
 end
@@ -36,6 +29,5 @@ hooksecurefunc("NPE_CheckTutorials", function()
 end)
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("VARIABLES_LOADED")
 f:SetScript("OnEvent", OnEvent)
