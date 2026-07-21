8bb8ba4e704729f5477c438a7c3c5f804175cf18
EK <eke00372@gmail.com>
2026-07-21 15:11:12 +0800

    Rework AutoLog and AutoInvite

--- !Anyon/Misc/AutoInvite.lua
+++ !Anyon/Misc/AutoInvite.lua
@@ -1,131 +1,165 @@
-local addon, ns = ... 
-local C, F, G, L = unpack(ns)
-if not C.AutoInvite then return end
+local _, ns = ...
+local F = ns[2]
+local M = F.RegisterModule("AutoInvite", "AutoInvite")
 
-local InviteKey = {"111", "+++", "inv",}
-local GuildInviteKey = {"g++", "ginv", "加公會", "加公会",}
-
+local format = string.format
 local strlower = string.lower
+local strmatch = string.match
+
+local BNInviteFriend = BNInviteFriend
+local CanCooperateWithGameAccount = CanCooperateWithGameAccount
+local ChatEdit_ClearChat = ChatEdit_ClearChat
+local CreateFrame = CreateFrame
+local hooksecurefunc = hooksecurefunc
+local IsAltKeyDown = IsAltKeyDown
+local IsControlKeyDown = IsControlKeyDown
+local IsInGroup = IsInGroup
+local IsModifierKeyDown = IsModifierKeyDown
+local StaticPopup_Show = StaticPopup_Show
+local UnitIsGroupAssistant = UnitIsGroupAssistant
+local UnitIsGroupLeader = UnitIsGroupLeader
+
 local C_BattleNet_GetAccountInfoByID = C_BattleNet.GetAccountInfoByID
+local GuildInvite = C_GuildInfo.Invite
 local InviteToGroup = C_PartyInfo.InviteUnit
 
--- [[ 按alt組隊邀請，ctrl公會邀請 ]] --
+local INVITE_KEYWORDS = {
+	["111"] = true,
+	["+++"] = true,
+	["inv"] = true,
+}
 
-hooksecurefunc("ChatFrame_OnHyperlinkShow", function(frame, link, _, button)
-	local type, value = link:match("(%a+):(.+)")
-	local hide
-	
-	if button == "LeftButton" and IsModifierKeyDown() then
-		if type == "player" then
-			local unit = value:match("([^:]+)")
-			if IsAltKeyDown() then
-				InviteToGroup(unit)
-				hide = true
-			elseif IsControlKeyDown() then
-				GuildInvite(unit)
-				hide = true
-			end
-		elseif type == "BNplayer" then
-			local _, bnID = value:match("([^:]*):([^:]*):")
-			if not bnID then return end
-			
-			local accountInfo = C_BattleNet_GetAccountInfoByID(bnID)
-			if not accountInfo then return end
-			
-			local gameAccountInfo = accountInfo.gameAccountInfo
-			local gameID = gameAccountInfo.gameAccountID
-			
-			if gameID then
-				if IsAltKeyDown() then
-					BNInviteFriend(gameID)
-					hide = true
-				elseif IsControlKeyDown() and CanCooperateWithGameAccount(accountInfo) then
-					local charName = gameAccountInfo.characterName
-					local realmName = gameAccountInfo.realmName
-					
-					GuildInvite(charName.."-"..realmName)
-					hide = true
-				end
-			end
-		end
-	else
-		return
+local GUILD_INVITE_KEYWORDS = {
+	["g++"] = true,
+	["ginv"] = true,
+	["加公會"] = true,
+	["加公会"] = true,
+}
+
+-- 戰網暱稱轉換為角色名字
+local function GetBattleNetGameAccount(presenceID)
+	local accountInfo = presenceID and C_BattleNet_GetAccountInfoByID(presenceID)
+	if not accountInfo or not CanCooperateWithGameAccount(accountInfo) then return end
+
+	local gameAccountInfo = accountInfo.gameAccountInfo
+	if not gameAccountInfo or not gameAccountInfo.gameAccountID then return end
+
+	return gameAccountInfo
+end
+
+-- 公會邀請獲取完整角色名
+local function GetBattleNetUnit(gameAccountInfo)
+	local charName = gameAccountInfo and gameAccountInfo.characterName
+	local realmName = gameAccountInfo and gameAccountInfo.realmName
+	if charName and realmName then
+		return charName.."-"..realmName
 	end
-	
-	-- 別打開輸入框
-	if hide then ChatEdit_ClearChat(ChatFrame1.editBox) end
-end)
+end
 
-StaticPopupDialogs["IOWguildinvPopup"] = {
-	--text = "Do you want to invite %s to your guild?",
+-- 只處理純文字
+local function GetKeyword(msg)
+	if type(msg) ~= "string" then return end
+	return strlower(msg)
+end
+
+-- 公會邀請確認視窗
+StaticPopupDialogs["ANYON_GUILD_INVITE"] = {
 	text = format(ERR_GUILD_INVITE_S, "%s"),
 	button1 = ACCEPT,
 	button2 = CANCEL,
-	OnAccept = function(self, data)
-		GuildInvite(data)
-	end,
-	OnCancel = function()
-		--do nuffin
-		return
+	OnAccept = function(_, unit)
+		if unit then
+			GuildInvite(unit)
+		end
 	end,
 	timeout = 0,
 	whileDead = true,
 	hideOnEscape = true,
-	preferredIndex = 3, 
+	preferredIndex = 3,
 }
 
--- [[ 密語關鍵字邀請 ]] --
+-- Alt 組隊邀請，Ctrl 公會邀請
+local function OnSetItemRef(link, _, button)
+	if button ~= "LeftButton" or not IsModifierKeyDown() then return end
 
-local WhisperInvite = CreateFrame("Frame", UIParent)
-	WhisperInvite:RegisterEvent("CHAT_MSG_WHISPER")
-	WhisperInvite:RegisterEvent("CHAT_MSG_BN_WHISPER")
-	-- EVENT 返回值 1密語 2角色id 12guid 13戰網好友的角色id
-	WhisperInvite:SetScript("OnEvent",function(self, event, msg, name, _, _, _, _, _, _, _, _, _, _, presenceID)
-		for _, word in pairs(InviteKey) do
-			if (not IsInGroup() or UnitIsGroupLeader("player") or UnitIsGroupAssistant("player")) and strlower(msg) == strlower(word) then
-				if event == "CHAT_MSG_BN_WHISPER" then
-					local accountInfo = C_BattleNet_GetAccountInfoByID(presenceID)
-					if accountInfo then
-						local gameAccountInfo = accountInfo.gameAccountInfo
-						local gameID = gameAccountInfo.gameAccountID
-						if gameID then
-							local charName = gameAccountInfo.characterName
-							local realmName = gameAccountInfo.realmName
-							--if CanCooperateWithGameAccount(accountInfo) then
-								BNInviteFriend(gameID)
-							--end
-						end
-					end
-				else
-					InviteToGroup(name)
+	local linkType, value = strmatch(link, "(%a+):(.+)")
+	if not linkType or not value then return end
+
+	local handled
+	if linkType == "player" then
+		local unit = strmatch(value, "([^:]+)")
+		if IsAltKeyDown() then
+			InviteToGroup(unit)
+			handled = true
+		elseif IsControlKeyDown() then
+			GuildInvite(unit)
+			handled = true
+		end
+	elseif linkType == "BNplayer" then
+		local _, bnID = strmatch(value, "([^:]*):([^:]*):")
+		local gameAccountInfo = GetBattleNetGameAccount(tonumber(bnID))
+		if gameAccountInfo then
+			if IsAltKeyDown() then
+				BNInviteFriend(gameAccountInfo.gameAccountID)
+				handled = true
+			elseif IsControlKeyDown() then
+				local unit = GetBattleNetUnit(gameAccountInfo)
+				if unit then
+					GuildInvite(unit)
+					handled = true
 				end
 			end
 		end
-		
-		for _, Gword in pairs(GuildInviteKey) do
-			if (not IsInGroup() or UnitIsGroupLeader("player") or UnitIsGroupAssistant("player")) and strlower(msg) == strlower(Gword) then
-				if event == "CHAT_MSG_BN_WHISPER" then
-					local accountInfo = C_BattleNet_GetAccountInfoByID(presenceID)
-					if accountInfo then
-						local gameAccountInfo = accountInfo.gameAccountInfo
-						local gameID = gameAccountInfo.gameAccountID
-						if gameID then
-							local charName = gameAccountInfo.characterName
-							local realmName = gameAccountInfo.realmName
-							if CanCooperateWithGameAccount(accountInfo) then
-								local dialog = StaticPopup_Show("IOWguildinvPopup", name)
-								if (dialog) then
-									dialog.data = charName.."-"..realmName
-								end
-							end
-						end
-					end
-				else
-					local dialog = StaticPopup_Show("IOWguildinvPopup", name)
-					if (dialog) then
-						dialog.data = name
-					end
-				end
-			end
+	end
+
+	if handled then
+		ChatEdit_ClearChat(ChatFrame1.editBox)
+	end
+end
+
+-- 密語關鍵字組隊邀請
+local function InviteByWhisper(event, author, presenceID)
+	if IsInGroup() and not UnitIsGroupLeader("player") and not UnitIsGroupAssistant("player") then return end
+
+	if event == "CHAT_MSG_BN_WHISPER" then
+		local gameAccountInfo = GetBattleNetGameAccount(presenceID)
+		if gameAccountInfo then
+			BNInviteFriend(gameAccountInfo.gameAccountID)
 		end
-	end)
+	else
+		InviteToGroup(author)
+	end
+end
+
+-- 密語關鍵字公會邀請
+local function GuildInviteByWhisper(event, author, presenceID)
+	local unit = author
+	if event == "CHAT_MSG_BN_WHISPER" then
+		unit = GetBattleNetUnit(GetBattleNetGameAccount(presenceID))
+	end
+	if unit then
+		StaticPopup_Show("ANYON_GUILD_INVITE", unit, nil, unit)
+	end
+end
+
+-- 只接受完全相同的關鍵字
+local function OnWhisper(_, event, ...)
+	local msg, author, _, _, _, _, _, _, _, _, _, _, presenceID = ...
+	local keyword = GetKeyword(msg)
+	if not keyword then return end
+
+	if INVITE_KEYWORDS[keyword] then
+		InviteByWhisper(event, author, presenceID)
+	elseif GUILD_INVITE_KEYWORDS[keyword] then
+		GuildInviteByWhisper(event, author, presenceID)
+	end
+end
+
+function M:OnEnable()
+	hooksecurefunc("SetItemRef", OnSetItemRef)
+
+	local whisperInvite = CreateFrame("Frame")
+	whisperInvite:RegisterEvent("CHAT_MSG_WHISPER")
+	whisperInvite:RegisterEvent("CHAT_MSG_BN_WHISPER")
+	whisperInvite:SetScript("OnEvent", OnWhisper)
+end
--- !Anyon/Misc/AutoLog.lua
+++ !Anyon/Misc/AutoLog.lua
@@ -1,42 +1,47 @@
-local addon, ns = ... 
-local C, F, G, L = unpack(ns)
-if not C.AutoLog then return end
+local _, ns = ...
+local F = ns[2]
+local M = F.RegisterModule("AutoLog", "AutoLog")
 
-local GetInstanceInfo, LoggingCombat = GetInstanceInfo, LoggingCombat
-local GetCVar, SetCVar = C_CVar.GetCVar, C_CVar.SetCVar
+local CreateFrame = CreateFrame
+local GetInstanceInfo = GetInstanceInfo
+local GetTime = GetTime
+local LoggingCombat = LoggingCombat
+local print = print
+
+local GetCVar = C_CVar.GetCVar
+local SetCVar = C_CVar.SetCVar
+
 local expirationTime
 
 local function autoLog()
-	-- Force enable advance combat logs
-	local cvar = GetCVar("advancedCombatLogging")
-	if cvar ~= 1 then
-		SetCVar("advancedCombatLogging", 1)
+	-- 強制開啟進階戰鬥紀錄
+	if GetCVar("advancedCombatLogging") ~= "1" then SetCVar("advancedCombatLogging", "1") end
+
+	-- 不要連續重試
+	if GetTime() - (expirationTime or 0) <= 0 then return end
+	-- 檢查紀錄是否開啟
+	local logging = LoggingCombat()
+	if logging == nil then
+		expirationTime = GetTime() + 10
+		return
 	end
-	
-	-- Enable combat logs
+
 	local _, instanceType, difficultyID = GetInstanceInfo()
-	if GetTime() - (expirationTime or 0) > 0 then
-		local loggingStatus = LoggingCombat()
-		if loggingStatus == nil then
-			expirationTime = GetTime() + 10
-		else
-			if (instanceType == "raid" or difficultyID == 8 or difficultyID == 23) then
-				if loggingStatus == false then
-					LoggingCombat(true)
-					print("|cff00FF00"..COMBATLOGENABLED.."|r")
-				end
-			else
-				if loggingStatus == true then
-					LoggingCombat(false)
-					print("|cffFF0000"..COMBATLOGDISABLED.."|r")
-				end
-			end
-		end
+	local shouldLog = instanceType == "raid" or difficultyID == 8 or difficultyID == 23
+
+	if shouldLog and (not logging) then
+		LoggingCombat(true)
+		print("|cff00FF00"..COMBATLOGENABLED.."|r")
+	elseif not shouldLog and logging then
+		LoggingCombat(false)
+		print("|cffFF0000"..COMBATLOGDISABLED.."|r")
 	end
 end
 
-local AL = CreateFrame("Frame")
-	AL:RegisterEvent("PLAYER_ENTERING_WORLD")
-	AL:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-	AL:RegisterEvent("CHALLENGE_MODE_START")
-	AL:SetScript("OnEvent", autoLog)
+function M:OnEnable()
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	frame:RegisterEvent("CHALLENGE_MODE_START")
+	frame:SetScript("OnEvent", autoLog)
+end
