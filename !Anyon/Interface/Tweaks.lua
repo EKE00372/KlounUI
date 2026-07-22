d6fe840a6415d2491befdc1133fad4cdded3151d
EK <eke00372@gmail.com>
2026-07-22 12:50:24 +0800

    Tweaks: add hide talent alert

--- !Anyon/Interface/Tweaks.lua
+++ !Anyon/Interface/Tweaks.lua
@@ -8,9 +8,28 @@ local DELETE_ITEM_CONFIRM_STRING = DELETE_ITEM_CONFIRM_STRING
 -- [[ Hide talent alerts ]]--
 
 do
-	function MainMenuMicroButton_AreAlertsEffectivelyEnabled()
-		return false
+	-- Function to hide talent alerts
+	local function HideAlert(microButton)
+		-- Only hide alerts for the PlayerSpellsMicroButton
+		if microButton == PlayerSpellsMicroButton then
+			MainMenuMicroButton_HideAlert(microButton)
+		end
 	end
+
+	-- Function to stop pulse animations on the talent button
+	local function HidePulse(microButton)
+		-- Only stop pulse for the PlayerSpellsMicroButton
+		if microButton == PlayerSpellsMicroButton then
+			MicroButtonPulseStop(microButton)
+		end
+	end
+
+	-- Hook the alert and pulse functions securely to avoid taint
+	hooksecurefunc("MainMenuMicroButton_ShowAlert", HideAlert)
+	hooksecurefunc("MicroButtonPulse", HidePulse)
+
+	-- Hides TutorialPointerFrame
+	SetCVar("showTutorials", 0)
 end
 
 -- [[ Bypass the buggy cancel cinematic confirmation dialog ]] --
